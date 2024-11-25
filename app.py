960b16f9c69c310866ce7f1f1e4d71cc879e3d70
Alex Bull <alexander.bull24@imperial.ac.uk>
2024-11-25 17:37:12 +0000

    Passing id to satellite.html

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+all_satellites = [
+    {
+        "id": "25544",
+        "name": "International Space Station",
+    },
+    {
+        "id": "20580",
+        "name": "Hubble Space Telescope",
+    }
+]
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/satellite", methods=["POST"])
+def satellite():
+    input_satellite = request.form.get("name")
+    for satellite in all_satellites:
+        if satellite["name"] == input_satellite:
+            id = satellite["id"]
+            return render_template("satellite.html", id=id)
+    return "404 Not Found", 404
+
+
 
 
 def process_query(query):
