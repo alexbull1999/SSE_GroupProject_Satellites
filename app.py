7b4aed31e208f0aaf3510cf9e4cbf73cfd2342fa
Alex Bull <alexander.bull24@imperial.ac.uk>
2024-11-26 17:21:18 +0000

    Adding API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import requests
+
 
 app = Flask(__name__)
 
@@ -24,10 +26,24 @@ def satellite():
     input_satellite = request.form.get("name")
     for satellite in all_satellites:
         if satellite["name"] == input_satellite:
-            return render_template("satellite.html", satellite=satellite)
+            id = satellite["id"]
+            url = f"https://api.n2yo.com/rest/v1/satellite/tle/{id}&apiKey=LMFEWE-UWEWBT-WF7CWC-5DK0"
+            response = requests.get(url)
+            if response.status_code == 200:
+                satellite_data = response.json()
+                return satellite_data
     return "404 Not Found", 404
 
 
 def process_query(query):
     if query.lower() == "moon":
         return "Moon made of cheese"
+
+
+# url = f"https://tle.ivanstanojevic.me/api/tle/{id}"
+# data = []
+# response = requests.get(url)
+# if response.status_code == 200:
+# data = response.json()
+
+#       return render_template("satellite.html", satellite=satellite_data)
