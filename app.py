0ecafa5a2cfb92b5cd60f185b3555233a793ca0c
Tim Jones <timothy.jones@imperial.ac.uk>
2024-11-28 10:44:43 +0000

    landing page clickable photos now work

--- app.py
+++ app.py
@@ -36,6 +36,19 @@ def satellite():
                 return satellite_data
     return "404 Not Found", 404
 
+@app.route("/satellite/<int:satellite_id>", methods=["GET"])
+def satellite_by_id(satellite_id):
+    #Search for the satellite by ID
+    start_url = "https://api.n2yo.com/rest/v1/satellite/tle/"
+    end_url = "&apiKey=LMFEWE-UWEWBT-WF7CWC-5DK0"
+    url = f"{start_url}{satellite_id}{end_url}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        satellite_data = response.json()
+        #change to return to the render template satellite.html
+        return satellite_data
+    return "404 Not Found", 404
+
 
 def process_query(query):
     if query.lower() == "moon":
