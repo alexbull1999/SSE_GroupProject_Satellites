82ec9e10015f4bbcbc3ab2cfc6c4300e3f996631
Alex Bull <alexander.bull24@imperial.ac.uk>
2024-11-28 14:30:42 +0000

    Adding satellite

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, MetaData
+
+metadata = MetaData()
+
+#define satellite table
+
+satelliteTable = Table(
+    'satellite',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(255), nullable=False),
+)
