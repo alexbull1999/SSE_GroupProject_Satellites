7e175168187e9379004bbf57ada3eaf7b15783a4
Sermila <sermila@hotmail.com>
2024-12-06 10:44:47 +0000

    blueprints working

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from database import (
     populate_country_table,
 )
 from dotenv import load_dotenv
-from blueprints import register_blueprints # Import the function to register blueprints
+from blueprints import (
+    register_blueprints,
+)  # Import the function to register blueprints
 
 load_dotenv()
 app = Flask(__name__)
@@ -21,15 +23,18 @@ if __name__ == "__main__":
     init_db(DATABASE_URL)
     populate_country_table("countries.csv", engine)
     app.run(debug=True)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/lookup")
 def lookup():
     return render_template("search.html")
 
+
 @app.route("/login_page")
 def login_page():
     return render_template("login.html")
-
